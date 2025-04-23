84fec1575f835631da8fb1781e61b819053efcb7
Fabian Castillo <fabianboc960@hotmail.com>
2025-04-23 10:43:08 -0500

    feath: :sparkles: se ajusta los DTOs de register y login y se implementa en el controlador

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto } from './dto/login.user.dto';
+import { RegisterUserDto } from './dto/register.user.dto';
 
 @Controller()
 export class AuthController {
@@ -8,13 +10,17 @@ export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @MessagePattern('auth.register.user')
-    register() {
-        return 'Register!!'
+    register(@Payload() registerUserDto: RegisterUserDto) {
+        return {
+            ...registerUserDto
+        }
     }
 
     @MessagePattern('auth.login.user')
-    login() {
-        return 'Login!!!'
+    login(@Payload() loginUserDto: LoginUserDto) {
+        return {
+            ...loginUserDto
+        }
     }
 
     @MessagePattern('auth.verify.user')
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { Logger } from '@nestjs/common';
-
+import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 
 	const logger = new Logger('AuthMS - Main');
@@ -16,6 +15,13 @@ async function bootstrap() {
 		},
 			
 		}
+	);
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
 	);
 
 	await app.listen();
