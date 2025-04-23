49337eddc7dbf2a1b385d1d27290b0d03f4cbc48
Fabian Castillo <fabianboc960@hotmail.com>
2025-04-23 10:09:00 -0500

    feat: :sparkles: Configuración del ms auth conectado a NATS
    Se realiza las conexiones necesarias para que entre a las conexiones de NATS, se implementa variables de entorno y se instalan dependencias

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,25 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+
+    constructor(private readonly authService: AuthService) { }
+
+    @MessagePattern('auth.register.user')
+    register() {
+        return 'Register!!'
+    }
+
+    @MessagePattern('auth.login.user')
+    login() {
+        return 'Login!!!'
+    }
+
+    @MessagePattern('auth.verify.user')
+    verify() {
+        return 'verify!!'
+    }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+	const logger = new Logger('AuthMS - Main');
+
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,
+		{
+			transport: Transport.NATS,
+			options: {
+				servers: envs.natsServer
+		},
+			
+		}
+	);
+
+	await app.listen();
+	logger.log(`Auth Microservice Running on port ${ envs.PORT }`);
 }
 bootstrap();
